296da7a2f072ad53cd95de1a2f2fdb7a8b04ecd7
Haninhafez <abdohanin058@gmail.com>
2025-08-23 16:08:07 -0400

    session 4 done

--- HomeWork_S4/Q11.dart
+++ HomeWork_S4/Q11.dart
@@ -1,14 +1,23 @@
 // ignore_for_file: dead_code
 
+import 'dart:io';
+
 void main() {
   double productPrice = 1000;
-  bool isStudent = true;
-  bool hasCoupon = false;
+  print("Are you Sturdent? true / false");
+  String Answer = stdin.readLineSync()!;
+  bool isStudent = (Answer.toLowerCase() == 'true');
+  bool? hasCoupon;
+  if (!isStudent) {
+    print("Do you have a coupon? true / false");
+    Answer = stdin.readLineSync()!;
+    hasCoupon = (Answer.toLowerCase() == 'true');
+  }
   double totalPrice;
 
   if (isStudent) {
     totalPrice = productPrice - (productPrice * .15);
-  } else if (hasCoupon) {
+  } else if (hasCoupon!) {
     totalPrice = productPrice - (productPrice * .10);
   } else if (productPrice > 1200) {
     totalPrice = productPrice - (productPrice * .5);
